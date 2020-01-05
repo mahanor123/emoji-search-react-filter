5a6e02be4c0e7a17c0c79093f7ac8190414379b5
Your Name <you@example.com>
2020-01-05 12:06:03 +0530

    added some css

--- src/components/forkRibbon.js
+++ src/components/forkRibbon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 function Ribbon(){
     return (
-        <a class="github-fork-ribbon" href="" title="Fork me on GitHub" style={{color: "white"}}>Fork me on GitHub</a>
+        <a class="github-fork-ribbon" href="https://github.com/mahanor123/emoji-search-react-filter" title="Fork me on GitHub" style={{color: "white"}}>Fork me on GitHub</a>
       
     )
 }
