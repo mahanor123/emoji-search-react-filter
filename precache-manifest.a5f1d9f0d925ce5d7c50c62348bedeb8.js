1c3caa829ec21b272860d27896c29a71e9654709
Your Name <you@example.com>
2020-01-29 10:59:48 +0530

    Updates

---
+++ precache-manifest.a5f1d9f0d925ce5d7c50c62348bedeb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d88fb42a11926c6793b7fe811ef8a0",
+    "url": "/mahanor123/emoji-search-react-filter/index.html"
+  },
+  {
+    "revision": "9bab4afcd590f8cc45d1",
+    "url": "/mahanor123/emoji-search-react-filter/static/css/main.45f7468e.chunk.css"
+  },
+  {
+    "revision": "687a7d2f7fb501b0c1e5",
+    "url": "/mahanor123/emoji-search-react-filter/static/js/2.305360be.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mahanor123/emoji-search-react-filter/static/js/2.305360be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bab4afcd590f8cc45d1",
+    "url": "/mahanor123/emoji-search-react-filter/static/js/main.7b179633.chunk.js"
+  },
+  {
+    "revision": "dbbc0aa407e9813fae0e",
+    "url": "/mahanor123/emoji-search-react-filter/static/js/runtime-main.386f95dd.js"
+  }
+]);
